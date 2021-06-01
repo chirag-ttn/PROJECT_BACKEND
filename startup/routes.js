f0d9721b1c7255106984eaeb267fc9eb4d895e5d
Chirag Gandhi <chirag.gandhi@tothenew.com>
2021-06-01 14:52:44 +0530

    done

--- startup/routes.js
+++ startup/routes.js
@@ -25,7 +25,8 @@ app.use('/api/auth',loginRouter)
 app.use('/profile',ProfileRouter)
 app.use('/posts',PostRouter)
 app.use('/users',UserRouter)
+console.log(__dirname.split('startup')[0]+'public/index.html')
 app.get('*',(req,res)=>{
-    res.sendFile('index.html')
+    res.sendFile(__dirname.split('startup')[0]+'public/index.html')
 })
 }
